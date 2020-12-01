887c8b4f1c677cb618621594ffaf26aa307c73a6
gianmarx <gianmarcogagliardi@webmapp.it>
2020-12-01 14:56:35 +0100

    table dashboard

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\AreaLayer;
 use App\Models\Poi;
+use App\Models\PoiLayer;
+use App\Models\TrackLayer;
 use App\Nova\Metrics\AreaLayerCount;
 use App\Nova\Metrics\AreaLayersPerDay;
 use App\Nova\Metrics\AreaLayersPerName;
@@ -13,6 +16,7 @@ use Illuminate\Support\Facades\Gate;
 use Laravel\Nova\Cards\Help;
 use Laravel\Nova\Nova;
 use Laravel\Nova\NovaApplicationServiceProvider;
+use MyApp\NovaTable\NovaTable;
 
 class NovaServiceProvider extends NovaApplicationServiceProvider
 {
@@ -64,6 +68,13 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
      */
     protected function cards()
     {
+        $pL = PoiLayer::paginate(10);
+        $tL = TrackLayer::paginate(10);
+        $aL = AreaLayer::paginate(10);
+
+        $header = collect(['Name', 'Description', 'Created_at', 'Updated_at']);
+
+
         return [
 
             new PoiLayerCount(),
@@ -71,6 +82,60 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
             new AreaLayerCount(),
             (new AreaLayersPerDay())->width('1/2'),
             (new AreaLayersPerName())->width('1/2'),
+            (new \Mako\CustomTableCard\CustomTableCard)
+                ->header([
+                    new \Mako\CustomTableCard\Table\Cell('Name'),
+                    new \Mako\CustomTableCard\Table\Cell('Description'),
+                    new \Mako\CustomTableCard\Table\Cell('Created_at'),
+                    new \Mako\CustomTableCard\Table\Cell('Updated_at'),
+                ])
+                ->data($pL->map(function ($order)
+                {
+                    return new \Mako\CustomTableCard\Table\Row(
+                        new \Mako\CustomTableCard\Table\Cell($order['name']),
+                        new \Mako\CustomTableCard\Table\Cell($order['description']),
+                        new \Mako\CustomTableCard\Table\Cell($order['created_at']),
+                        new \Mako\CustomTableCard\Table\Cell($order['updated_at']));
+
+                })->toArray())
+                ->title('Poi Layers')
+                ->viewall(['label' => 'View All', 'link' => '/nova/resources/poi-layers']),
+            (new \Mako\CustomTableCard\CustomTableCard)
+                ->header([
+                    new \Mako\CustomTableCard\Table\Cell('Name'),
+                    new \Mako\CustomTableCard\Table\Cell('Description'),
+                    new \Mako\CustomTableCard\Table\Cell('Created_at'),
+                    new \Mako\CustomTableCard\Table\Cell('Updated_at'),
+                ])
+                ->data($tL->map(function ($order)
+                {
+                    return new \Mako\CustomTableCard\Table\Row(
+                        new \Mako\CustomTableCard\Table\Cell($order['name']),
+                        new \Mako\CustomTableCard\Table\Cell($order['description']),
+                        new \Mako\CustomTableCard\Table\Cell($order['created_at']),
+                        new \Mako\CustomTableCard\Table\Cell($order['updated_at']));
+
+                })->toArray())
+                ->title('Area Layers')
+                ->viewall(['label' => 'View All', 'link' => '/nova/resources/track-layers']),
+            (new \Mako\CustomTableCard\CustomTableCard)
+                ->header([
+                    new \Mako\CustomTableCard\Table\Cell('Name'),
+                    new \Mako\CustomTableCard\Table\Cell('Description'),
+                    new \Mako\CustomTableCard\Table\Cell('Created_at'),
+                    new \Mako\CustomTableCard\Table\Cell('Updated_at'),
+                ])
+                ->data($tL->map(function ($order)
+                {
+                    return new \Mako\CustomTableCard\Table\Row(
+                        new \Mako\CustomTableCard\Table\Cell($order['name']),
+                        new \Mako\CustomTableCard\Table\Cell($order['description']),
+                        new \Mako\CustomTableCard\Table\Cell($order['created_at']),
+                        new \Mako\CustomTableCard\Table\Cell($order['updated_at']));
+
+                })->toArray())
+                ->title('Track Layers')
+                ->viewall(['label' => 'View All', 'link' => '/nova/resources/area-layers']),
         ];
     }
 
