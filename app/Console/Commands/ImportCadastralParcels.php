18282899037ccc39caee7c76ec2f252b61b1a438
Davide Pizzato <davidepizzato@webmapp.it>
2021-10-15 12:22:54 +0200

    Added file reading

--- app/Console/Commands/ImportCadastralParcels.php
+++ app/Console/Commands/ImportCadastralParcels.php
@@ -3,6 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use Symfony\Component\Console\Command\Command as CommandAlias;
 
 class ImportCadastralParcels extends Command {
@@ -11,14 +14,15 @@ class ImportCadastralParcels extends Command {
      *
      * @var string
      */
-    protected $signature = 'sisteco:import_cadastral_parcels';
+    protected $signature = 'sisteco:import_cadastral_parcels
+        {--geojson=imports/cadastral_parcels.geojson : The geojson file location}';
     /**
      * The console command description.
      *
      * @var string
      */
     protected $description = 'Import all the cadastral parcels from a well formatted geojson.
-        By default it searches the geojson in storage/app/imports/cadastral_parcels.geojson';
+        Current test file needs at least 1G of php memory limit';
 
     /**
      * Create a new command instance.
@@ -33,8 +37,15 @@ class ImportCadastralParcels extends Command {
      * Execute the console command.
      *
      * @return int
+     * @throws FileNotFoundException
      */
     public function handle(): int {
+        $geojsonUri = $this->option('geojson');
+        $geojson = Storage::disk('local')->get($geojsonUri);
+
+        $geojson = json_decode($geojson, true);
+        Log::info(count($geojson));
+
         return CommandAlias::SUCCESS;
     }
 }
