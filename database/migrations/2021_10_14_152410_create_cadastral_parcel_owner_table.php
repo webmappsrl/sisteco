9ec75b7fe25ff33ca88e72ef37f9d54d1f2ca3c8
Davide Pizzato <davidepizzato@webmapp.it>
2021-10-15 09:44:27 +0200

    Fixed migrations naming

--- database/migrations/2021_10_14_152410_create_cadastral_parcel_owner_table.php
+++ database/migrations/2021_10_14_152410_create_cadastral_parcel_owner_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CadastralParcelClient extends Migration {
+class CreateCadastralParcelOwnerTable extends Migration {
     /**
      * Run the migrations.
      *
--- database/migrations/2021_10_14_152508_create_cadastral_parcel_land_use_table.php
+++ database/migrations/2021_10_14_152508_create_cadastral_parcel_land_use_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CadastralParcelLandUse extends Migration {
+class CreateCadastralParcelLandUseTable extends Migration {
     /**
      * Run the migrations.
      *
