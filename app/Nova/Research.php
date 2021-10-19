d3ba99eee2db916e74ad55f7d5b289ab6b04ca9e
Davide Pizzato <davidepizzato@webmapp.it>
2021-10-19 11:19:45 +0200

    Added research resource fields

--- app/Nova/Research.php
+++ app/Nova/Research.php
@@ -3,88 +3,94 @@
 namespace App\Nova;
 
 use Illuminate\Http\Request;
+use Laravel\Nova\Fields\BelongsToMany;
 use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Number;
+use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
-class Research extends Resource
-{
+class Research extends Resource {
     /**
      * The model the resource corresponds to.
      *
      * @var string
      */
     public static $model = \App\Models\Research::class;
-
     /**
      * The single value that should be used to represent the resource when being displayed.
      *
      * @var string
      */
-    public static $title = 'id';
-
+    public static $title = 'name';
     /**
      * The columns that should be searched.
      *
      * @var array
      */
     public static $search = [
-        'id',
+        'name',
     ];
 
     /**
      * Get the fields displayed by the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
+     *
      * @return array
      */
-    public function fields(Request $request)
-    {
+    public function fields(Request $request): array {
         return [
-            ID::make(__('ID'), 'id')->sortable(),
+            Text::make('Title'),
+            Text::make('Description')->onlyOnDetail(),
+            Number::make('Number of cadastral parcels', function ($model) {
+                return count($model->cadastralParcels);
+            }),
+            Text::make('Filters')->onlyOnDetail(),
+            BelongsToMany::make('Cadastral parcels', 'cadastralParcels')->onlyOnDetail(),
         ];
     }
 
     /**
      * Get the cards available for the request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
+     *
      * @return array
      */
-    public function cards(Request $request)
-    {
+    public function cards(Request $request) {
         return [];
     }
 
     /**
      * Get the filters available for the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
+     *
      * @return array
      */
-    public function filters(Request $request)
-    {
+    public function filters(Request $request) {
         return [];
     }
 
     /**
      * Get the lenses available for the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
+     *
      * @return array
      */
-    public function lenses(Request $request)
-    {
+    public function lenses(Request $request) {
         return [];
     }
 
     /**
      * Get the actions available for the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
+     *
      * @return array
      */
-    public function actions(Request $request)
-    {
+    public function actions(Request $request) {
         return [];
     }
 }
--- database/factories/ResearchFactory.php
+++ database/factories/ResearchFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\CadastralParcel;
 use App\Models\Research;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -14,14 +15,26 @@ class ResearchFactory extends Factory {
     protected $model = Research::class;
 
     /**
+     * Configure the model factory.
+     *
+     * @return $this
+     */
+    public function configure(): ResearchFactory {
+        return $this->afterCreating(function (Research $model) {
+            $ids = CadastralParcel::limit(10)->get()->pluck('id');
+            $model->cadastralParcels()->sync($ids);
+        });
+    }
+
+    /**
      * Define the model's default state.
      *
      * @return array
      */
     public function definition(): array {
         return [
-            'title' => $this->faker->title,
-            'description' => $this->faker->text
+            'title' => $this->faker->sentence(3),
+            'description' => $this->faker->text()
         ];
     }
 }
