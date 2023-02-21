f558794dcf29b1b046da1635e4e3ee5b18315207
Alessio Piccioli <piccioli@netseven.it>
2023-02-21 05:56:49 +0100

    Model (-a) Catalog, CatalogType, CatalogArea created

---
+++ app/Http/Controllers/CatalogAreaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CatalogArea;
+use Illuminate\Http\Request;
+
+class CatalogAreaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CatalogArea  $catalogArea
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CatalogArea $catalogArea)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\CatalogArea  $catalogArea
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CatalogArea $catalogArea)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CatalogArea  $catalogArea
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CatalogArea $catalogArea)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CatalogArea  $catalogArea
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CatalogArea $catalogArea)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/CatalogTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CatalogType;
+use Illuminate\Http\Request;
+
+class CatalogTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CatalogType  $catalogType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CatalogType $catalogType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\CatalogType  $catalogType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CatalogType $catalogType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CatalogType  $catalogType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CatalogType $catalogType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CatalogType  $catalogType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CatalogType $catalogType)
+    {
+        //
+    }
+}
